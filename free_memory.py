42fca85f646cc74627a53f24dcac40ab24dc340a
ambrosio <sgtambrosio2019@gmail.com>
2026-02-04 15:07:49 -0300

    implementação de função

--- free_memory.py
+++ free_memory.py
@@ -1,3 +1,8 @@
 def memory():
 	"""Retorna o uso de memoria do computador"""    
-	print("Verificando a memoria do computador!")
+	memory = psutil.virtual_memory()
+	print(f"Uso de memória: {memory.percent}%")
+	print(f"Memória total: {memory.total / 1024**3:.2f} GB")
+	print(f"Memória disponível: {memory.available / 1024**3:.2f} GB")
+	print(f"Memória usada: {memory.used / 1024**3:.2f} GB")
+

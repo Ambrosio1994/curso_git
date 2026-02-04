1de36be3a8bf3a6ab0f34b018ba16ebcfa432c1c
ambrosio <sgtambrosio2019@gmail.com>
2026-02-04 15:04:49 -0300

    update docstring

--- free_memory.py
+++ free_memory.py
@@ -1,5 +1,3 @@
 def memory():
-    print("Verificando a memoria do computador!")
-// https://github.com/Ambrosio1994
-if __name__ == "__main__":
-    memory()
+	"""Retorna o uso de memoria do computador"""    
+	print("Verificando a memoria do computador!")
